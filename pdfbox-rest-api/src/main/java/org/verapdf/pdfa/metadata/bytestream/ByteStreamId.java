354873748dccd7c44d92e36313f45676fe0acefc
Carl Wilson <carl@openplanetsfoundation.org>
2014-12-25 12:30:11 +0000

    Updated JavaDoc use of SHA256.

--- pdfbox-rest-api/src/main/java/org/verapdf/pdfa/metadata/bytestream/ByteStreamId.java
+++ pdfbox-rest-api/src/main/java/org/verapdf/pdfa/metadata/bytestream/ByteStreamId.java
@@ -8,20 +8,16 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 
 /**
- * Encapsulates a few byte stream properties:
+ * Encapsulates two byte stream properties:
  * <ul>
  * <li>The length of the ByteStream in bytes, will be >= 0.</li>
- * <li>The hex encoded MD5 digest calculated from the ByteStream, a 32 character
- * hex string, i.e. [0-9a-f]{32}.</li>
- * <li>The hex encoded SHA256 digest calculated from the ByteStream, a 64
- * character hex string, i.e. [0-9a-f]{64}.</li>
+ * <li>The hex encoded SHA1 digest calculated from the ByteStream, a 40
+ * character hex string, i.e. [0-9a-f]{40}.</li>
  * </ul>
  * Used as an identifier for tagging richer metadata and associating it with a
  * ByteStream.
  * 
- * @author <a href="mailto:carl@openplanetsfoundation.org">Carl Wilson</a> <a
- *         href="https://github.com/carlwilson">carlwilson AT github</a>
- * @version 0.1 Created 02 Jul 2012:02:37:33
+ * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>
  */
 @JsonSerialize(as = ByteStreamIdImpl.class)
 @JsonDeserialize(as = ByteStreamIdImpl.class)
@@ -33,7 +29,7 @@ public interface ByteStreamId {
 
 	/**
 	 * @return The hex encoded SHA1 digest calculated from the ByteStream, a
-	 *         50 character hex string, i.e. [0-9a-f]{64}.
+	 *         40 character hex string, i.e. [0-9a-f]{40}.
 	 */
 	public String getHexSHA1();
 
