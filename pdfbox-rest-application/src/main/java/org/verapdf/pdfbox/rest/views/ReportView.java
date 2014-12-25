cee830dd53dc658f9a8c69b7730c8d4c62a417eb
Carl Wilson <carl@openplanetsfoundation.org>
2014-12-25 12:41:31 +0000

    Improved JavaDoc for class and methods.

--- pdfbox-rest-application/src/main/java/org/verapdf/pdfbox/rest/views/ReportView.java
+++ pdfbox-rest-application/src/main/java/org/verapdf/pdfbox/rest/views/ReportView.java
@@ -8,15 +8,22 @@ import io.dropwizard.views.View;
 import org.verapdf.pdfa.metadata.ValidationReport;
 
 /**
- * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>.</p>
- *
+ * Dropwizard {@link io.dropwizard.views.View View} to generate an HTML
+ * representation of a {@link org.verapdf.pdfa.metadata.ValidationReport PDF/A
+ * validation report}. This uses the mustache template reportfragment.mustache
+ * mapped from the main resources folder matching the View's package in this
+ * case org.verapdf.pdfbox.rest.views.
+ * 
+ * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>.
  */
 public class ReportView extends View {
 
     private ValidationReport report;
+
     /**
-     * @param report 
-     * 
+     * @param report
+     *            the {@link org.verapdf.pdfa.metadata.ValidationReport
+     *            ValidationReport} to display as HTML
      */
     public ReportView(ValidationReport report) {
         super("reportfragment.mustache"); //$NON-NLS-1$
@@ -24,7 +31,9 @@ public class ReportView extends View {
     }
 
     /**
-     * @return
+     * @return the {@link org.verapdf.pdfa.metadata.ValidationReport
+     *         ValidationReport}, required by mustache to access report data in
+     *         template
      */
     public ValidationReport getReport() {
         return this.report;
