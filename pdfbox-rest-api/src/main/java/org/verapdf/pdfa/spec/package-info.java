4f60d82ad709687a2bdf041b13a6a851541f97ad
Carl Wilson <carl@openplanetsfoundation.org>
2014-12-29 12:32:24 +0000

    Created new package for spec model.

---
+++ pdfbox-rest-api/src/main/java/org/verapdf/pdfa/spec/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Package to hold a set of enums that identify the individual elements of the
+ * PDF/A Specifications.
+ * 
+ * In order to assess and report test coverage of the specifications the specs
+ * themselves need to be first-class citizens in the code. These are supplied as
+ * a set of enums that create a hierarchical tree structure from specification
+ * down to numbered sub-section.
+ * 
+ * @author <a href="mailto:carl@openpreservation.org">Carl Wilson</a>.</p>
+ */
+package org.verapdf.pdfa.spec;
