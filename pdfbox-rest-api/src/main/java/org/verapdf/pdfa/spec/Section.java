e9077151d27cc1e90d55a76cd84d79f70addbfa2
Carl Wilson <carl@openplanetsfoundation.org>
2015-01-02 01:45:51 +0000

    Refactoring section Ids.

--- pdfbox-rest-api/src/main/java/org/verapdf/pdfa/spec/Section.java
+++ pdfbox-rest-api/src/main/java/org/verapdf/pdfa/spec/Section.java
@@ -10,94 +10,122 @@ import java.util.SortedSet;
  *
  */
 public interface Section extends Comparable<Section> {
-    /**
-     * @return the identifier of the specification section, in a normal section
-     *         sub-section form, i.e. n.n.n where n is numberic.
-     */
-    public Id getId();
+	/**
+	 * @return the identifier of the specification section, in a normal section
+	 *         sub-section form, i.e. n.n.n where n is numberic.
+	 */
+	public Id getId();
 
-    /**
-     * @return a descriptive name for the section
-     */
-    public String getName();
+	/**
+	 * @return a descriptive name for the section
+	 */
+	public String getName();
 
-    /**
-     * @return the full title, which is the id and name
-     */
-    public String getTitle();
+	/**
+	 * @return the full title, which is the id and name
+	 */
+	public String getTitle();
 
-    /**
-     * @return the parent section
-     */
-    public Section getParent();
+	/**
+	 * @return the parent section
+	 */
+	public Section getParent();
 
-    /**
-     * @return true if this section has no parents, i.e. it's the root
-     */
-    public boolean isRoot();
+	/**
+	 * @return true if this section has no parents, i.e. it's the root
+	 */
+	public boolean isRoot();
 
-    /**
-     * @param isChild
-     *            Section object to be checked to see if it's a sub-section of
-     *            this section
-     * 
-     * @return true if isChild is a direct child of this Section
-     */
-    public boolean isParentOf(Section isChild);
+	/**
+	 * @param isChild
+	 *            Section object to be checked to see if it's a sub-section of
+	 *            this section
+	 * 
+	 * @return true if isChild is a direct child of this Section
+	 */
+	public boolean isParentOf(Section isChild);
 
-    /**
-     * @param isDescendant
-     *            Section object to be checked to see if it's in the descendants
-     *            tree at all
-     * @return true if isDescendant is a sub-section of this section or its
-     *         sub-sections
-     */
-    public boolean isAncestorOf(Section isDescendant);
+	/**
+	 * @param isDescendant
+	 *            Section object to be checked to see if it's in the descendants
+	 *            tree at all
+	 * @return true if isDescendant is a sub-section of this section or its
+	 *         sub-sections
+	 */
+	public boolean isAncestorOf(Section isDescendant);
 
-    /**
-     * @param isChild
-     *            Section object to be checked to see if it's a sub-section of
-     *            this section
-     * 
-     * @return true if isChild is a direct child of this Section
-     */
-    public boolean isParentOf(Id isChild);
+	/**
+	 * @return any child sub-sections
+	 */
+	public SortedSet<Section> getSubSections();
 
-    /**
-     * @param isDescendant
-     *            Section object to be checked to see if it's in the descendants
-     *            tree at all
-     * @return true if isDescendant is a sub-section of this section or its
-     *         sub-sections
-     */
-    public boolean isAncestorOf(Id isDescendant);
+	/**
+	 * @param subSection
+	 *            adds the subsection in natural ordering
+	 * @return true if sub-section added successfully
+	 */
+	public boolean addSubSection(Section subSection);
 
-    /**
-     * @return any child sub-sections
-     */
-    public SortedSet<Section> getSubSections();
+	/**
+	 * @param toFind
+	 * @return the sub-section with id equal to find or null if no such
+	 *         subsection exists
+	 */
+	public Section getSubSection(Id toFind);
 
-    /**
-     * @param subSection
-     *            adds the subsection in natural ordering
-     * @return true if sub-section added successfully
-     */
-    public boolean addSubSection(Section subSection);
+	/**
+	 * Behaviour of a PDF/A specification section ID. More generally it models
+	 * document heading style id's of the form
+	 * <ul>
+	 * <li>1 First Heading</li>
+	 * <li>1.1 First Sub Heading</li>
+	 * <li>2 Second Heading</li>
+	 * <ul>
+	 * where the IDs are the numeric parts for each heading,
+	 */
+	public static interface Id extends Comparable<Id> {
+		/**
+		 * @return the String value of the Section id
+		 */
+		public String getValue();
 
-    /**
-     * @param toFind
-     * @return the sub-section with id equal to find or null if no succh
-     *         subsection exists
-     */
-    public Section getSubSection(Id toFind);
+		/**
+		 * @return the individual int elements that make the id
+		 */
+		public int[] getIntParts();
 
-    /**
-     *
-     */
-    public static interface Id extends Comparable<Id> {
-        /**
-         * @return the String value of the Section id
-         */
-        public String getValue();
-    }
+		/**
+		 * @return true if this section has no parents, i.e. it's the root
+		 */
+		public boolean isRoot();
+
+		/**
+		 * @param isChild
+		 *            Id object to be checked to see if it's a hild of this id
+		 * 
+		 * @return true if isChild is a direct child of this Section
+		 */
+		public boolean isParentOf(Id isChild);
+
+		/**
+		 * @param isDescendant
+		 *            Id object to be checked to see if it's in the descendants
+		 *            tree tree of this id
+		 * @return true if isDescendant is a decendant of this id
+		 */
+		public boolean isAncestorOf(Id isDescendant);
+
+		/**
+		 * @return the Parent ID of this ID or itself if this is a root id
+		 */
+		public Id getParentId();
+
+		/**
+		 * @param ordinal
+		 *            the int ordingal for the new id
+		 * @return a new Id that is a child of this one, i.e. the new id is
+		 *         parent.id + ',' + ordinal
+		 */
+		public Id createChildId(int ordinal);
+	}
 }
