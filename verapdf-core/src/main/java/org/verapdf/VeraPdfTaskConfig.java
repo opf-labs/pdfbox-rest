494185320c2499ee4b8ecde143fc80cf4f488d22
Carl Wilson <carl@openplanetsfoundation.org>
2015-01-28 10:53:50 +0300

    Added hashCode, equals, and JavaDoc.

--- verapdf-core/src/main/java/org/verapdf/VeraPdfTaskConfig.java
+++ verapdf-core/src/main/java/org/verapdf/VeraPdfTaskConfig.java
@@ -92,6 +92,59 @@ public final class VeraPdfTaskConfig {
 		return this.stopErrors;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (this.fixMetadata ? 1231 : 1237);
+		result = prime * result
+				+ ((this.flavour == null) ? 0 : this.flavour.hashCode());
+		result = prime * result + this.stopErrors;
+		result = prime * result + (this.validate ? 1231 : 1237);
+		result = prime * result + this.verbosity;
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof VeraPdfTaskConfig)) {
+			return false;
+		}
+		VeraPdfTaskConfig other = (VeraPdfTaskConfig) obj;
+		if (this.fixMetadata != other.fixMetadata) {
+			return false;
+		}
+		if (this.flavour != other.flavour) {
+			return false;
+		}
+		if (this.stopErrors != other.stopErrors) {
+			return false;
+		}
+		if (this.validate != other.validate) {
+			return false;
+		}
+		if (this.verbosity != other.verbosity) {
+			return false;
+		}
+		return true;
+	}
+
 	/**
 	 * @return the default VeraPdfTaskConfig instance
 	 */
@@ -100,25 +153,48 @@ public final class VeraPdfTaskConfig {
 	}
 
 	/**
+	 * Factory method used to create VeraPdfTaskConfig instances from values.
+	 * This method will check the arguments against illegal values and throw the
+	 * appropriate runtime errors.
+	 * 
 	 * @param flavour
+	 *            enumeration indicating the PDF/A flavour to validate against
 	 * @param validate
+	 *            boolean set true to request PDF/A validation, false disable.
 	 * @param fixMetadata
+	 *            boolean set true to request metadata fixes, false to disable.
 	 * @param verbosity
+	 *            integer for requesting verbosity level, should be in range 0 -
+	 *            9
 	 * @param stopErrors
-	 * @return
+	 *            requests that validation be stopped after encountering this
+	 *            number of errors, 0 indicates don't stop and show all errors
+	 * @return an immutable VeraPdfTaskConfig object populated from the values
+	 * @throws NullPointerException
+	 *             if the PdfaFlavour flavour argument is null.
+	 * @throws IllegalArgumentException
+	 *             if the int verbosity is outside the range 0-9
+	 * @throws IllegalArgumentException
+	 *             if the int stopErrors is negative.
 	 */
-	public final static VeraPdfTaskConfig fromValues(
-			final PdfaFlavour flavour, final boolean validate,
-			final boolean fixMetadata, final int verbosity, final int stopErrors) {
+	public final static VeraPdfTaskConfig fromValues(final PdfaFlavour flavour,
+			final boolean validate, final boolean fixMetadata,
+			final int verbosity, final int stopErrors) {
 		// Check that flavour is not null
-		Preconditions.checkNotNull(flavour, "Expected PdfaFlavour to be non-null");
+		Preconditions.checkNotNull(flavour,
+				"Expected PdfaFlavour to be non-null");
 		// Check that verbosity is between 0 - 9
-		Preconditions.checkArgument(((verbosity >= VERBOSITY_MIN) && (verbosity <= VERBOSITY_MAX)),
-				"Expected verbosity >= %s and <= %s, but %s is outside range.", Integer.valueOf(VERBOSITY_MIN), Integer.valueOf(VERBOSITY_MAX), Integer.valueOf(verbosity));
+		Preconditions.checkArgument(
+				((verbosity >= VERBOSITY_MIN) && (verbosity <= VERBOSITY_MAX)),
+				"Expected verbosity >= %s and <= %s, but %s is outside range.",
+				Integer.valueOf(VERBOSITY_MIN), Integer.valueOf(VERBOSITY_MAX),
+				Integer.valueOf(verbosity));
 		// Check that stopErrors >= 0
 		Preconditions.checkArgument((stopErrors >= 0),
-				"Expected stopErrors to be >= %s but is %s", Integer.valueOf(STOPERRORS_DEFAULT), Integer.valueOf(stopErrors));
-		return new VeraPdfTaskConfig(flavour, validate, fixMetadata,
-				verbosity, stopErrors);
+				"Expected stopErrors to be >= %s but is %s",
+				Integer.valueOf(STOPERRORS_DEFAULT),
+				Integer.valueOf(stopErrors));
+		return new VeraPdfTaskConfig(flavour, validate, fixMetadata, verbosity,
+				stopErrors);
 	}
 }
